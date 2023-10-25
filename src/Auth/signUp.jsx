6928fdff4587c565491f53932ce7669086437376
Bablukmr <bablukmr800@gmail.com>
2023-10-25 21:24:34 +0530

    logo

--- src/Auth/signUp.jsx
+++ src/Auth/signUp.jsx
@@ -10,7 +10,7 @@ function SignUp() {
 
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center">
-      <h1 className="text-2xl my-4 absolute top-[90px]">LOGO</h1>
+      <h1 className="text-2xl my-4 absolute top-[60px]">LOGO</h1>
       <div className="w-[80%] md:w-[50%] lg:w-[35%]">
         <form className="w-full flex flex-col items-center justify-center gap-3">
           <div className="w-full">
