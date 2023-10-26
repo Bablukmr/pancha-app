477b3a17849d2819c1f02ab93d7325ffc011d0c0
Bablukmr <bablukmr800@gmail.com>
2023-10-26 18:50:23 +0530

    token

--- src/Auth/authRouts.jsx
+++ src/Auth/authRouts.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import SignUp from './signUp'
-// import NotFound from '../notFound'
+import NotFound from '../notFound'
 import RequestPassword from './requestPassword'
 import Login from './logIn'
 
@@ -8,10 +8,10 @@ function AuthRouts() {
   return (
     <>
     <Routes>
-      <Route path='/' element={<Login/>} />
+      <Route path="/" element={<Login/>} />
       <Route path="signup" element={<SignUp/>} />
       <Route path="request" element={<RequestPassword/>} />
-      {/* <Route path="/*" element={<NotFound />} /> */}
+      <Route path="/*" element={<NotFound />} />
     </Routes>
     </>
   )
