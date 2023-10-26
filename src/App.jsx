1301e9f2953ee9fccbbc7e8557b255dac80476a0
Bablukmr <bablukmr800@gmail.com>
2023-10-26 18:42:20 +0530

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./App/appLayout";
 import ProtectedRoute from "./protectedRoute";
-import AuthLayout from "./Auth/authLayout";
 import { Provider } from "react-redux";
 import { useStore } from "./store/store";
+import AuthRouts from "./Auth/authRouts";
 
 function App(props) {
 const store = useStore(props.initialReduxState);
@@ -12,7 +12,7 @@ const store = useStore(props.initialReduxState);
     <Provider store={useStore(store)}>
       <BrowserRouter>
         <Routes>
-          <Route path="auth/*" element={<AuthLayout />} />
+          <Route path="auth/*" element={<AuthRouts />} />
           <Route path="/*" element={<ProtectedRoute Component={AppLayout} />} />
         </Routes>
       </BrowserRouter>
--- src/Auth/authRouts.jsx
+++ src/Auth/authRouts.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import SignUp from './signUp'
-import NotFound from '../notFound'
+// import NotFound from '../notFound'
 import RequestPassword from './requestPassword'
 import Login from './logIn'
 
@@ -11,7 +11,7 @@ function AuthRouts() {
       <Route path='/' element={<Login/>} />
       <Route path="signup" element={<SignUp/>} />
       <Route path="request" element={<RequestPassword/>} />
-      <Route path="/*" element={<NotFound />} />
+      {/* <Route path="/*" element={<NotFound />} /> */}
     </Routes>
     </>
   )
