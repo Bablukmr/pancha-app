b216d15a3ba40d36a5734d0963d0adaf7f8ab8e1
Bablukmr <bablukmr800@gmail.com>
2023-10-26 16:35:54 +0530

    library Page

--- src/App/libraryPage.jsx
+++ src/App/libraryPage.jsx
@@ -6,7 +6,7 @@ function LibraryPage() {
   return (
     <div className="w-full flex mt-[70px] flex-col items-center justify-center">
       <h1 className="text-2xl my-6">Library</h1>
-<div className="w-[80%]  border-black border-b-2 ">
+<div className="w-[80%] md:w-[50%] lg:w-[35%] border-black border-b-2 ">
             <p className="text-md my-2">Curated Folder</p>
           <div className="p-2 w-full border border-[#725555] pb-2 bg-[#fafafa] rounded-md mb-6">
             <ul className="w-full max-h-[150px]  overflow-y-scroll flex flex-col items-start  ">
@@ -23,7 +23,7 @@ function LibraryPage() {
           </div>
       
 </div>
-<div className="w-[80%] mt-2 border-black border-b-2">
+<div className="w-[80%] md:w-[50%] lg:w-[35%]  mt-2 border-black border-b-2">
             <p className="text-md my-2"> My Folder</p>
           <div className="p-2 w-full border border-[#725555] pb-2 bg-[#fafafa] rounded-md mb-6">
             <ul className="w-full max-h-[150px]  overflow-y-scroll flex flex-col items-start ">
@@ -40,9 +40,9 @@ function LibraryPage() {
           </div>
       
 </div>
-<div className="w-[80%] mt-4 flex flex-col items-start justify-start gap-3 mb-[70px]">
-<button className="bg-yellow-500 text-white rounded-md py-2 px-4">Add New Folder</button>
-<button className="bg-red-500 text-white rounded-md py-2 px-4">View/Edit Folder</button>
+<div className="w-[80%] md:w-[50%] lg:w-[35%]  mt-4 flex flex-col items-start justify-start gap-3 mb-[70px]">
+<button className="bg-[#A0E200] text-white rounded-md py-2 px-4">Add New Folder</button>
+<button className="bg-[#E2202C] text-white rounded-md py-2 px-4">View/Edit Folder</button>
   <ButtonComponent bg="white" text="white" btnName="View Folder in Flash Mode"/>
 </div>
     </div>
